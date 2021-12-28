d2f5d23659e1e5c6dc648bdf1b49f771879f8c22
ceewee <itsmeceewee@gmail.com>
2021-12-28 19:38:30 +0800

    update files

---
+++ run.py
@@ -0,0 +1,4 @@
+from __init__ import app
+
+app = create_app()
+app.run(host="127.0.0.1", port=5000, debug=True)
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FLASK_APP=wordsearch-app
-cd /home/ec2-user
-flask run
